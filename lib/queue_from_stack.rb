e7fadd7bb0e16e13ed7e60d38d611530f35edb00
Nikan <njavadzadeh@gmail.com>
2022-12-17 20:48:37 -0800

    commit tree 6cae6384c3d6ba69d3c2340620444bf413d6e791

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,39 @@
 require_relative './stack'
 
+
+class MyQueue
+
+
+    def initialize
+    @s1= Stack.new
+    @s2= Stack.new
+    end
+
+    def add(value)
+        @s1.push(value)
+    end
+
+    def remove 
+        move_elements_from_s1_to_s2
+        @s2.pop
+    end
+
+    def peek
+        move_elements_from_s1_to_s2
+        @s2.peek
+    end
+    private
+
+    def move_elements_from_s1_to_s2
+
+        until @s1.empty?
+            @s2.push(@s1.pop)
+
+        end
+    end
+
+
+end
+
+
 # your code here
